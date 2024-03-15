6c3fcf96649f4a027a5c9ccf6997ddd5552f8c6d
Jkaps11 <kapsjosh11@gmail.com>
2024-03-15 17:54:50 -0400

    Finished basic functionality, needs to be tested

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,14 @@ use::std::fs::{File, DirBuilder, write, read_to_string};
 const EXECUTABLES_FILE: &str = "../executables.json";
 const EXECUTABLES_DIRECTORY: &str = "../executables_dir";
 
-#[derive(Serialize, Deserialize)]
+
+fn check_os(){
+    if cfg!(not(target_os = "windows")) {
+        panic!("This CLI tool only works on windows for now. Stay tuned for updates!")
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone)]
 struct ExecutableData {
     name: String,
     keyword: String,
@@ -63,38 +70,65 @@ impl FileData {
         }
     }
 
-    fn add_executable(&mut self, executable_data: ExecutableData) {
-        self.executables.insert(executable_data.keyword, executable_data);
-    }
+    fn add_executable(&mut self, executable_name: &str, keyword: &str) {
+        let new_executable_data = ExecutableData::new(executable_name, keyword);
+        match self.executables.insert(new_executable_data.keyword.clone(), new_executable_data.clone()){
+            None => println!("executable {} added successfully with keyword {}", new_executable_data.name, new_executable_data.keyword),
+            Some(old_exe_data) => println!("keyword {} that pointed to executable {} now points to executable {}", new_executable_data.keyword, old_exe_data.name, new_executable_data.name),
+        };
 
-    fn increment_num_executables(&mut self) {
         self.num_executables += 1;
     }
-}
+    
+    fn remove_executable(&mut self, keyword: &str){
+        match self.executables.remove(keyword) {
+            None => panic!("No executable with keyword {} was found", keyword),
+            Some(exe_data) => println!("Executable {} with keyword {} succesfully removed", exe_data.name, keyword),
+        };
 
-// handles saving FileData to json
-pub fn save_data_to_executable_json(current_file_data: &FileData) {
-    match write(EXECUTABLES_FILE, current_file_data.to_json()) {
-        Ok(_) => (),
-        Err(e) => panic!("Unable to write FileData to File {}", e),
-    };
-}
+        self.num_executables -= 1;
+    }
 
-// handles creation of the file containing the names of the executable files and their keywords
-// checks to see if the files is already there before creation
-pub fn create_executable_file(current_file_data: &mut FileData) {
-    match Path::new(EXECUTABLES_FILE).try_exists() {
-        // not first time running
-        Ok(true) => *current_file_data = FileData::get_file_data(),
-        
-        //first time running
-        Ok(false) => {
-            match File::create(EXECUTABLES_FILE) {
-                Ok(_) => (),
-                Err(e) => panic!("Error {} with file creation", e),
+    fn launch_executable(&mut self, keyword: &str) {
+       match self.executables.get_mut(keyword) {
+            None => panic!("No executable found for keyword {}", keyword),
+            Some(exe_data) => {
+                exe_data.increment_num_times_opened();
+                let executable_path = format!("{}/{}", EXECUTABLES_DIRECTORY, exe_data.name);
+                // command to launch lnk file on windows 10: START <path>
+                std::process::Command::new("START")
+                    .arg(executable_path)
+                    .status()
+                    .expect("failed to launch executable");
             }
-        },
-        Err(e) => panic!("There was an error checking if the Exectuables json exists {}", e),
+        };
+    }
+
+    // handles saving FileData to json
+    fn save_data_to_executable_json(&self) {
+        match write(EXECUTABLES_FILE, self.to_json()) {
+            Ok(_) => println!("Your configuration was successfully saved!"),
+            Err(e) => panic!("Unable to write FileData to File {}", e),
+        };
+    }
+
+
+    // handles creation of the file containing the names of the executable files and their keywords
+    // checks to see if the files is already there before creation
+    pub fn create_executable_file(&mut self) {
+        match Path::new(EXECUTABLES_FILE).try_exists() {
+            // not first time running
+            Ok(true) => *self = FileData::get_file_data(),
+        
+            //first time running
+            Ok(false) => {
+                match File::create(EXECUTABLES_FILE) {
+                    Ok(_) => println!("Executable JSON file created"),
+                    Err(e) => panic!("Error {} with file creation", e),
+                }
+            },
+            Err(e) => panic!("There was an error checking if the Exectuables json exists {}", e),
+        };
     }
 }
 
@@ -103,32 +137,18 @@ pub fn create_executable_file(current_file_data: &mut FileData) {
 // dirbuilder create automatically checks if the directory already exists
 pub fn create_executable_directory() {
     match DirBuilder::new().create(EXECUTABLES_DIRECTORY) {
-        Ok(_) => (),
+        Ok(_) => println!("Executables Directory initialized"),
         Err(_) => panic!("There was an error checking if the Executables directory exitsts"),
-    }
-}
-
-// handles adding an executable to executables json and updating metadata
-pub fn add_executable(exec: &str, keyword: &str) {
-
-}
-
-// handles removing an executable forom executables json and updating metadata
-pub fn remove_executable(keyword: &str) {
-
-}
-
-// handles launching executable by seraching through executables json and finding the correct
-// executable. Then it looks for it int the executables directory and launches it
-pub fn launch_executable(path: &str) {
-
+    };
 }
 
 fn main() {
 
+    check_os();
+
     let current_file_data: &mut FileData = &mut FileData::new();
 
-    create_executable_file(current_file_data);
+    current_file_data.create_executable_file();
     create_executable_directory();
 
     // CLI setup using clap
@@ -167,19 +187,19 @@ fn main() {
     if let Some(add_args) = match_result.subcommand_matches("add") {
         let executable_to_add = add_args.get_one::<String>("executable").unwrap();
         let keyword_to_add = add_args.get_one::<String>("keyword").unwrap();
-        add_executable(executable_to_add, keyword_to_add);
+        current_file_data.add_executable(executable_to_add, keyword_to_add);
     }
 
     if let Some(launch_args) = match_result.subcommand_matches("launch") {
         let keyword_to_search = launch_args.get_one::<String>("keyword").unwrap();
-        launch_executable(keyword_to_search);
+        current_file_data.launch_executable(keyword_to_search);
     }
 
     if let Some(remove_args) = match_result.subcommand_matches("remove") {
         let keyword_to_search = remove_args.get_one::<String>("keyword").unwrap();
-        remove_executable(keyword_to_search);
+        current_file_data.remove_executable(keyword_to_search);
     }
     
-    save_data_to_executable_json(current_file_data);
+    current_file_data.save_data_to_executable_json();
 }
 
